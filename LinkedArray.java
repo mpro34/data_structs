7bdbcac411c42237e6408d34e07eeb7b3052b9f9
Chris Whiting <chris.whiting@panasonic.aero>
2016-11-15 14:13:05 -0800

    Added an insert method to Node and rearranged into a separate LinkedListNew class

--- LinkedArray.java
+++ LinkedArray.java
@@ -2,20 +2,67 @@
 
 
 class LinkedArray {
+    public static void main(String [] args) {
+        System.out.println("Hello, World");
+        LinkedListNew ll = new LinkedListNew("r");
+        ll.insert("h");
+        ll.insert("p");
+        ll.insert("c");
+        ll.insert(7);
+        System.out.println(ll.printList());
+    }
+}
 
+class LinkedListNew {
+    private Node rootnode;
+    
+    LinkedListNew() {
+        rootnode = new Node();
+    }
+    
+    LinkedListNew(Object o) {
+        rootnode = new Node(o);
+    }
+    
+    public void insert(Object data) {
+        Node n = rootnode;
+        while (n.nextnode != null) {
+            n = n.nextnode;
+            n.counter++;
+        }
+        n.nextnode = new Node(data);
+    }
+    
+    public String printList() {
+        Node n = rootnode;
+        String list = "";
+        if (n.nextnode == null) { list += "["+n.getData()+"]["+n.getNextNode()+"] -> "; }
+        while (n.nextnode != null) {
+            list += "["+n.getData()+"]["+n.getNextNode()+"] -> ";
+            n = n.nextnode;
+        }
+        if (n.nextnode == null) { list += "["+n.getData()+"]["+n.getNextNode()+"]"; }
+        return list;
+    }
+    
     public static void main(String[] args) {
-        Node n = new Node();
-        Node c = new Node("n");
-        int num = (int)n.getData();
-        String hes = (String)c.getData();
+       // for (int i = 0; i < 11; ++i) {
+      //      Node n = new Node(i);
+            
+      //  }
+     //   Node n = new Node();
+      ///  Node c = new Node("n");
+     //   Node c2 = new Node("c");
+     //   c.insert(c2);
+      //  int num = (int)n.getData();
+       // String hes = (String)c.getData();
         
-        System.out.println("num: "+num+", hes: "+hes);
+        System.out.println("inside!");
     }
     
     public static class Node {
         private Object data;
         private Node nextnode;
-        private Node rootnode;
         private static int counter;
         
         public Node() {
@@ -38,9 +85,6 @@ class LinkedArray {
             return nextnode;
         }
         
-        public void insertNode(Node n) {
-            for (int i = 0; i < counter; ++i) {
-            }
-        }
+
     }
 }
